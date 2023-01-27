e6727e4e930f36c0632850197d7f9e4dee20641f
Craig Walker <craig.walker@methods.co.uk>
2023-01-27 10:51:54 +0000

    Testing TFLint

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 terraform {
+  backend "gcs" {}
   required_version = "1.3.7"
   required_providers {
     google = {
@@ -20,4 +21,16 @@ provider "google" {
 provider "google-beta" {
   project = var.project_id
   region  = var.region
+}
+
+module "activate_apis" {
+  source  = "terraform-google-modules/project-factory/google//modules/project_services"
+  version = "14.1.0"
+
+  project_id = var.project_id
+
+  activate_apis = [
+    "compute.googleapis.com",
+    "iam.googleapis.com",
+  ]
 }
